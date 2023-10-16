f84811d51b668124beb1616388a1674c84891813
Minasuki <jossiel2023@gmail.com>
2023-10-16 01:02:36 -0700

    Theme casi listo

---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import { useTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import ColorModeContext from "./ColorMode";
+import { principal } from "./styles";
+
+const ThemeSwitcher = () => {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+  return (
+    <Box
+      sx={principal}
+    >
+      {theme.palette.mode} mode
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default ThemeSwitcher;
